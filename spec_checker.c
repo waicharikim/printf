868ddeb9c81f2ce3c976e6d3af47ba350437792c
waicharikim <waicharikimani@gmail.com>
2023-06-29 10:57:06 +0300

    fixed array iterator to include new members of the struct

---
+++ spec_checker.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+/**
+ * spec_check - checks for format specifier
+ * @c: characterto check
+ *
+ * Return: pointer to function that matches the format specifier
+
+ */
+
+int (*spec_check(const char *c))(va_list)
+{
+	int i;
+	op_t spec_list[6] = {
+		{"c", _printc},
+		{"s", _prints},
+		{"%", _printpc},
+		{"d", _printd},
+		{"i", _printd},
+		{"b", _printb}};
+
+		for (i = 0; i < 6; i++)
+		{
+			if (*(spec_list[i].op) == *c)
+			{
+				return (spec_list[i].f);
+
+			}
+		}
+	return(NULL);
+}
